745b05fe6f68d57e4a35f380cdb276400e8b1a48
DevJac <dbuttonz@gmail.com>
2023-10-18 14:58:57 -0600

    TODO comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// TODO: Game struct
+// TODO: Game.new
+// TODO: Game.run ?
+// TODO: Game.process_input
+// TODO: Game.update
+// TODO: Game.render
+// TODO: How will I play sounds?
+// TODO: Quit on window manager signal and when escape is pressed
+// TODO: Clear window with a color
+// TODO: I will need to track keystate myself, possible with a set
+// TODO: Simulate a lower resolution
+
 fn main() {
+    // TODO: Process input
+    // TODO: Update game state
+    // TODO: Render
     println!("Hello, world!");
 }
