27adb5815b06d85e953f6e35c2ba3e61f878aba3
DevJac <dbuttonz@gmail.com>
2023-11-06 10:27:04 -0700

    Tank moves across screen

--- src/main.rs
+++ src/main.rs
@@ -15,24 +15,31 @@ use pikuma_game_engine::renderer;
 
 struct Game {
     renderer: renderer::Renderer,
+    tank_location: glam::Vec2,
 }
 
 impl Game {
     fn new(window: winit::window::Window, width: u32, height: u32) -> Self {
         let renderer = renderer::Renderer::new(window, width, height);
         renderer.configure_surface();
-        Game { renderer }
+        Game {
+            renderer,
+            tank_location: glam::Vec2::new(-20.0, 25.0),
+        }
     }
 
     fn configure_surface(&self) {
         self.renderer.configure_surface();
     }
 
-    fn render(&mut self, delta_t: std::time::Duration) {
-        self.renderer
-            .draw_image(renderer::TankOrTree::Tank, glam::UVec2::new(40, 25));
+    fn render(&mut self, delta_t: f32) {
+        self.tank_location += glam::Vec2::new(50.0, 0.0) * delta_t;
         self.renderer
             .draw_image(renderer::TankOrTree::Tree, glam::UVec2::new(20, 10));
+        self.renderer.draw_image(
+            renderer::TankOrTree::Tank,
+            self.tank_location.round().as_uvec2(),
+        );
         self.renderer.draw();
     }
 }
@@ -47,6 +54,7 @@ fn main() {
     let mut game = Game::new(window, 800, 600);
     let start_time = std::time::Instant::now();
     let mut last_render_time = start_time;
+    let mut frame_render_seconds: f32 = 0.0;
     let mut last_fps_log_time = start_time;
     let mut render_time_stats = FPSStats::new(1.0);
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
@@ -91,10 +99,10 @@ fn main() {
                     // TODO: Track button states
                 }
                 winit::event::Event::AboutToWait => {
-                    game.render(time_since_start);
+                    game.render(frame_render_seconds);
                     let now = std::time::Instant::now();
-                    let render_time_seconds: f32 = (now - last_render_time).as_secs_f32();
-                    render_time_stats.update(render_time_seconds);
+                    frame_render_seconds = (now - last_render_time).as_secs_f32();
+                    render_time_stats.update(frame_render_seconds);
                     last_render_time = now;
                     if now - last_fps_log_time > std::time::Duration::from_secs(10) {
                         last_fps_log_time = now;
