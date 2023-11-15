cc3a5382548fe3cc9cb958c90874d83bbcbf598c
DevJac <dbuttonz@gmail.com>
2023-11-15 16:35:04 -0700

    Registry.add_component

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,3 +1,9 @@
+#[derive(Debug)]
+enum DeadEntity {
+    DeadEntity,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Entity {
     id: usize,
     generation: usize,
@@ -10,6 +16,10 @@ struct FreeEntityIds {
 impl FreeEntityIds {
     fn new() -> Self {
         Self { ids: Vec::new() }
+    }
+
+    fn push(&mut self, id: usize) {
+        self.ids.push(id)
     }
 
     fn pop(&mut self) -> Option<usize> {
@@ -28,31 +38,73 @@ impl EntityGenerations {
         }
     }
 
-    fn get(&mut self, entity_id: usize) -> usize {
+    fn get(&self, entity_id: usize) -> usize {
+        if entity_id >= self.generations.len() {
+            return 0;
+        }
+        self.generations[entity_id]
+    }
+
+    fn increment(&mut self, entity_id: usize) -> usize {
         if entity_id >= self.generations.len() {
             // We make room for several extra entity ids to avoid
             // increasing the capacity by 1 over and over
             // and thus causing lots of copying.
             self.generations.resize(entity_id + 10, 0);
         }
-        dbg!(entity_id, self.generations.len());
         debug_assert!(entity_id < self.generations.len());
+        self.generations[entity_id] += 1;
         self.generations[entity_id]
+    }
+
+    fn is_alive(&self, entity: Entity) -> bool {
+        let entity_id = entity.id;
+        let alive_generation: usize;
+        if entity_id >= self.generations.len() {
+            alive_generation = 0;
+        } else {
+            debug_assert!(entity_id < self.generations.len());
+            alive_generation = self.generations[entity_id];
+        }
+        entity.generation == alive_generation
     }
 }
 
-struct ComponentPool<T> {
+struct ComponentPool<T: Clone> {
     components: Vec<Option<T>>,
 }
 
-impl<T> ComponentPool<T> {}
+impl<T: Clone> ComponentPool<T> {
+    fn new_one(entity_id: usize, component: T) -> Self {
+        // We make room for several extra components to avoid
+        // increasing the capacity by 1 over and over
+        // and thus causing lots of copying.
+        let mut components = vec![None; 10];
+        components[entity_id] = Some(component);
+        Self { components }
+    }
+
+    fn get(&self, entity_id: usize) -> Option<&T> {
+        if entity_id >= self.components.len() {
+            return None;
+        }
+        self.components[entity_id].as_ref()
+    }
+
+    fn set(&mut self, entity_id: usize, component: T) {
+        if entity_id >= self.components.len() {
+            // We make room for several extra components to avoid
+            // increasing the capacity by 1 over and over
+            // and thus causing lots of copying.
+            self.components.resize(entity_id + 10, None);
+        }
+        self.components[entity_id] = Some(component);
+    }
+}
 
 struct Registry {
     /// The maximum entity id we have issued. This is the "length" of the Registry.
-    max_entity_id: usize,
-    /// The maximum number of entities we are capable of issuing.
-    /// This will need to grow when max_entity_id becomes large enough.
-    entity_capacity: usize,
+    next_entity_id: usize,
     /// Entity ids that are free to issue again.
     free_entity_ids: FreeEntityIds,
     /// The current generation of the entities.
@@ -66,8 +118,7 @@ struct Registry {
 impl Registry {
     fn new() -> Self {
         Self {
-            max_entity_id: 0,
-            entity_capacity: 100,
+            next_entity_id: 0,
             free_entity_ids: FreeEntityIds::new(),
             entity_generations: EntityGenerations::new(),
             component_pools: std::collections::HashMap::new(),
@@ -82,11 +133,48 @@ impl Registry {
             };
         }
         let new_entity = Entity {
-            id: self.max_entity_id,
+            id: self.next_entity_id,
             generation: 0,
         };
-        self.max_entity_id += 1;
+        self.next_entity_id += 1;
         new_entity
+    }
+
+    fn is_entity_alive(&self, entity: Entity) -> bool {
+        self.entity_generations.is_alive(entity)
+    }
+
+    fn remove_entity(&mut self, entity: Entity) -> Result<(), DeadEntity> {
+        if !self.is_entity_alive(entity) {
+            return Err(DeadEntity::DeadEntity);
+        }
+        self.entity_generations.increment(entity.id);
+        self.free_entity_ids.push(entity.id);
+        Ok(())
+    }
+
+    fn add_component<T: Clone + 'static>(
+        &mut self,
+        entity: Entity,
+        component: T,
+    ) -> Result<(), DeadEntity> {
+        if !self.is_entity_alive(entity) {
+            return Err(DeadEntity::DeadEntity);
+        }
+        let type_id = (&component as &dyn std::any::Any).type_id();
+        match self.component_pools.get_mut(&type_id) {
+            None => {
+                let new_component_pool =
+                    Box::new(ComponentPool::<T>::new_one(entity.id, component));
+                self.component_pools.insert(type_id, new_component_pool);
+            }
+            Some(component_pool) => {
+                let component_pool: &mut ComponentPool<T> =
+                    (&mut **component_pool).downcast_mut().unwrap();
+                component_pool.set(entity.id, component);
+            }
+        }
+        Ok(())
     }
 }
 
@@ -96,13 +184,40 @@ fn test_entity_generations_happy_path() {
     assert_eq!(eg.get(0), 0);
     assert_eq!(eg.get(1), 0);
     assert_eq!(eg.get(10), 0);
+    assert_eq!(eg.get(5), 0);
+    eg.increment(5);
+    eg.increment(1);
+    assert_eq!(eg.get(0), 0);
+    assert_eq!(eg.get(1), 1);
+    assert_eq!(eg.get(10), 0);
+    assert_eq!(eg.get(5), 1);
 
     let mut eg = EntityGenerations::new();
-    assert_eq!(eg.get(10), 0);
+    assert_eq!(eg.get(100), 0);
+    eg.increment(100);
+    assert_eq!(eg.get(100), 1);
 }
 
 #[test]
 fn test_registry_happy_path() {
     let mut registry: Registry = Registry::new();
-    registry.create_entity();
+    let e0: Entity = registry.create_entity();
+    let e1: Entity = registry.create_entity();
+
+    let e2: Entity = registry.create_entity();
+    registry.add_component(e2, 5_i32).unwrap();
+
+    assert_eq!(registry.next_entity_id, 3);
+    registry.remove_entity(e0).unwrap();
+    registry.remove_entity(e1).unwrap();
+    assert_eq!(registry.next_entity_id, 3);
+    let _e0: Entity = registry.create_entity();
+    let _e1: Entity = registry.create_entity();
+    assert_eq!(registry.next_entity_id, 3);
+    let _e3: Entity = registry.create_entity();
+    assert_eq!(registry.next_entity_id, 4);
+
+    registry.add_component(e2, 5_i32).unwrap();
+    registry.remove_entity(e2).unwrap();
+    assert!(registry.add_component(e2, 5_i32).is_err());
 }
